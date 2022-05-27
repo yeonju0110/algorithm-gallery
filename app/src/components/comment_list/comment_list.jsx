543a958c8ff31cff24076eefc54d7a80b5f0532a
Yeonju Jo <yjj0287@gmail.com>
2022-05-27 20:42:18 +0900

    feat: post view component

---
+++ app/src/components/comment_list/comment_list.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CommentItem from '../comment_item/comment_item';
+import styles from './comment_list.module.css';
+
+const CommentList = ({ comments }) => {
+    return (
+        <ul className={styles.box}>
+            {comments.map(comment => (
+                <CommentItem
+                    key={comment.commentid}
+                    comment={comment} 
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default CommentList;
