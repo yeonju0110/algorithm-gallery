0d41293ed7c274143b9c0f316294d3c4730429be
NaA-hwang <pgup10753@gmail.com>
2022-05-27 17:21:31 +0900

    Add post icon

---
+++ app/src/components/Post_Icon/Post_Icon.jsx
@@ -0,0 +1,12 @@
+import { ReactDOM } from "react";
+import styles from './Post_Icon.module.css';
+
+function Fixed_Post_Icon() {
+    return (
+        <>
+            <a href='' className={styles.iconImg}></a>
+        </>
+    )
+}
+
+export default Fixed_Post_Icon;
