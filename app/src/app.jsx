531b85bdf4bf82480165098a670ced0b7e98d206
NaA-hwang <pgup10753@gmail.com>
2022-05-27 14:49:26 +0900

    comit before Update from Upstream

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './app.module.css';
+import Field_Tags from './components/Field_Tags/Field_Tags';
+import Field_PostEdit from './components/Field_PostEdit/Field_PostEdit';
+import Field_PostPublish from './components/Field_PostPublish/Field_PostPublish';
 
 function App() {
-  return <h1>Hello안녕</h1>;
+  return (
+    <>
+      {/* 
+      <Field_Tags />
+      <Field_PostEdit />
+      */}
+      <Field_PostPublish />
+    </>
+  );
 }
 
 export default App;
