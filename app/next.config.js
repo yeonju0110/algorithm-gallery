ae86496d02d852164f07c005582fdd90dc8e9d08
mjmj0217 <mjmj0217@naver.com>
2022-07-06 22:44:08 +0900

    feat: env in next

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,7 @@
 const removeImports = require("next-remove-imports")();
 module.exports = removeImports({
-    experimental: { esmExternals: true }
+    experimental: { esmExternals: true },
+    env: {
+        REACT_APP_ALG_SERVER: process.env.REACT_APP_ALG_SERVER,
+    },
 });
